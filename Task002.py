00e25e85a6a7c968f64885c9ef53c79dff52da79
Yakovlev Dima <zif18@rambler.ru>
2022-12-18 16:44:39 +0500

    Task002 changed

--- Task002.py
+++ Task002.py
@@ -5,8 +5,11 @@
 import os
 os.system('cls||clear')
 a = int(input('Введите трехзначное число \nа= '))
-result = 0
-while (a != 0) :
-    result = result + a % 10
-    a //= 10
-print ('Сумма чисел в составе числа a = ', result)
+if len(str(a)) == 3:
+    result = 0
+    while (a != 0) :
+        result = result + a % 10
+        a //= 10
+    print ('Сумма чисел в составе числа a = ', result)
+else :
+    print ('Введено не корректное число!!')
